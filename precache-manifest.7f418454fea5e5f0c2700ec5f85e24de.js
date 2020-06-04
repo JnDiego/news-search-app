a687b51d2041d23c51deb5d40dc5b4971fdd86ce
Juan Diego <diego_suarez@outlook.com>
2020-06-04 18:53:34 -0500

    Updates

---
+++ precache-manifest.7f418454fea5e5f0c2700ec5f85e24de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4af840996a26b209e24b4ede2ec85d",
+    "url": "/news-search-app/index.html"
+  },
+  {
+    "revision": "a73b4df503f0b09dee3e",
+    "url": "/news-search-app/static/css/main.b50117a7.chunk.css"
+  },
+  {
+    "revision": "f4d6cc5e673b1bf57c78",
+    "url": "/news-search-app/static/js/2.1eaac33d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-search-app/static/js/2.1eaac33d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a73b4df503f0b09dee3e",
+    "url": "/news-search-app/static/js/main.726aca6f.chunk.js"
+  },
+  {
+    "revision": "4e67f86906fe34a12770",
+    "url": "/news-search-app/static/js/runtime-main.2c82b158.js"
+  }
+]);
